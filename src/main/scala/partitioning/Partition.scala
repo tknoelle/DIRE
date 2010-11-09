83315ca019d7eadff828f5cc49fba1ec18c2976e
tk <tk@tklaptop.localdomain>
2010-11-09 19:07:01 +0100

    modified:   src/main/scala/partitioning/Partition.scala

--- src/main/scala/partitioning/Partition.scala
+++ src/main/scala/partitioning/Partition.scala
@@ -33,9 +33,10 @@ class Partition extends ClauseStoragePartitioning with Logging{
       i = i+1
     }      */
 
-    val g = newGraph(getEdgeWeight(module0), getNodeWeight(module0))
-    printGraph(g, "/home/tk/hiwi/DIRE/input/conf/output.dire")
-
+    val n = getNodeWeight(module0)
+    val g = newGraph(getEdgeWeight(module0), n)
+    printGraph(g, "/home/tk/hiwi/DIRE/input/conf/output")
+    printPredicates(n, "/home/tk/hiwi/DIRE/input/conf/output")
 
     module0.forall({clause: FOLClause => clause.literals.exists(
       {literal : FOLNode => (literal match {
@@ -399,7 +400,7 @@ class Partition extends ClauseStoragePartitioning with Logging{
    */
   def printGraph(g: List[Edge], file: String) = {
     var i = 0
-    val bufferedWriter = new BufferedWriter(new FileWriter(file))
+    val bufferedWriter = new BufferedWriter(new FileWriter(file+".dire"))
     try {
       bufferedWriter.write("list_of_clauses().\n")
       while(i<g.length){
@@ -411,6 +412,24 @@ class Partition extends ClauseStoragePartitioning with Logging{
         i = i+1
       }
       bufferedWriter.write("end_of_list. ")
+    } finally {
+      try {
+      bufferedWriter.close()
+      } catch { case _ => }
+    }
+  }
+
+  def printPredicates(p: List[Node], file:String){
+    var i = 1
+    var pr = p
+    val bufferedWriter = new BufferedWriter(new FileWriter(file+".net"))
+    try {
+      bufferedWriter.write("*Vertices "+p.size +"\n")
+      while(!pr.isEmpty){
+        bufferedWriter.write(i+" "+pr.head.getName +"\n")
+        i = i+1
+        pr = pr.tail
+      }
     } finally {
       try {
       bufferedWriter.close()
